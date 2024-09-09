36cc025e13171027ce9d4a6514219f281ca32590
GabrielRomeroM <gabrielromeromaidana@gmail.com>
2024-09-09 14:26:10 -0300

    V0.3

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-document.getElementById('card-form').onsubmit = function (event) {
-    event.preventDefault();
+document
+    .getElementById('card-form')
+        .addEventListener = ('submit', function (event){
+        event.preventDefault();
 
-    const nombre = document.getElementById('card-name').value;
-    const apellidos = document.getElementById('card-last-name').value;
-    const username = document.getElementById('card-user').value;
-    const avatarUrl = document.getElementById('card-image').value || `https://avatar.iran.liara.run/public`;
-    const bgColor = document.getElementById('card-bg-color').value;
-    const borderColor = document.getElementById('card-border-color').value;
+        const nombre = document.getElementById('card-name').value;
+        const apellidos = document.getElementById('card-last-name').value;
+        const username = document.getElementById('card-user').value;
+        const avatarUrl = document.getElementById('card-image').value || `https://avatar.iran.liara.run/public`;
+        const bgColor = document.getElementById('card-bg-color').value;
+        const borderColor = document.getElementById('card-border-color').value;
 
-    const cardHtml = `
-        <div class="card">
+        const cardContainer = document.getElementById('cards-container');
+        const newCard = document.createElement('div');
+        newCard.className = 'card';
+        newCard.style.backgroundColor = bgColor;
+        newCard.style.borderColor = borderColor;
+
+        newCard.innerHTML = `
+            <div class="card">
             <img src="${avatarUrl}" alt="Avatar">
             <div>
                 <h3>${apellidos} ${nombre}</h3>
@@ -17,6 +25,7 @@ document.getElementById('card-form').onsubmit = function (event) {
             </div>
         </div>
     `;
+    cardContainer.appendChild(newCard);
 
-    document.getElementById('cards-container').innerHTML += cardHtml;
-};
+    document.getElementById('card-form').reset();
+});

27cf0e6eee1092a289f4809c6a8ed8dc0c6415c2
GabrielRomeroM <gabrielromeromaidana@gmail.com>
2024-09-11 18:56:30 -0300

    V2.1

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ document
         event.preventDefault();
 
         const nombre = document.getElementById('card-name').value;
-        const apellidos = document.getElementById('card-last-name').value;
-        const username = document.getElementById('card-user').value;
+        const tiltle = document.getElementById('card-last-name').value;
+        const description = document.getElementById('card-user').value;
         const avatarUrl = document.getElementById('card-image').value || `https://avatar.iran.liara.run/public`;
         const bgColor = document.getElementById('card-bg-color').value;
         const borderColor = document.getElementById('card-border-color').value;
 
         console.log({
             nombre, 
-            apellidos, 
-            username, 
+            tiltle, 
+            description, 
             avatarUrl, 
             bgColor, 
             borderColor});
@@ -26,8 +26,9 @@ document
         newCard.innerHTML = `
             <img src="${avatarUrl}" alt="Avatar">
             <div class="card-content">
-                <h3>${apellidos} ${nombre}</h3>
-                <p>@${username}</p>
+                <h3>${tiltle}</h3> 
+                <h3>${nombre}</h3>
+                <p>@${description}</p>
             </div>
         `;
         cardContainer.appendChild(newCard);
